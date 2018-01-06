2ad1c807b85f5cbd339810fa0918508dbccebf2e
xFyne <yannikschaefr@aol.de>
2018-01-06 15:32:10 +0100

    new activity PowerActivity, to start the App/TV

---
+++ app/src/main/java/com/example/tvcontroller/PowerActivity.java
@@ -0,0 +1,13 @@
+package com.example.tvcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PowerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_power);
+    }
+}
