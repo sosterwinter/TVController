ab6c9e1290728b6c35f6658cdebc2c55600239dd
xFyne <yannikschaefr@aol.de>
2018-01-06 15:59:19 +0100

    On Click event for PowerActivity

--- app/src/main/java/com/example/tvcontroller/PowerActivity.java
+++ app/src/main/java/com/example/tvcontroller/PowerActivity.java
@@ -1,7 +1,9 @@
 package com.example.tvcontroller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PowerActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class PowerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power);
+    }
+
+    public void onClickButton(View view){
+
+        //Senderliste hier laden
+
+
+        Intent start_main_activity = new Intent(this, MainActivity.class);
+        startActivity(start_main_activity);
+
     }
 }
