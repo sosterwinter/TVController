85d943334bdff636f7c267bac5a6a01e890346bc
sosterwinter <s.osterwinter@gmx.de>
2018-01-14 02:25:22 +0100

    File einlesen/ausgeben/deleten auf managefavorites

--- app/src/main/java/com/example/tvcontroller/MainActivity.java
+++ app/src/main/java/com/example/tvcontroller/MainActivity.java
@@ -18,6 +18,14 @@ import android.widget.SeekBar;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
 
     //Creating member variables
@@ -30,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
 
     HttpRequest httpReq;
     Singleton singleton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         this.httpReq = new HttpRequest("10.0.2.2", 1000, false);
         //this.task = new HttpRequestAsync(this.httpReq);
         this.singleton = Singleton.getInstance();
+        String filename;
         //Initialisierung
 
         //TV-Server initialisieren
@@ -174,6 +182,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
